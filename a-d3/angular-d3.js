bde9073f6b8f888cc8d546d7f82c52bab33aec91
Florian Reisinger <reisi007@gmail.com>
2016-03-13 09:36:52 +0100

    Improve code quality

--- a-d3/angular-d3.js
+++ a-d3/angular-d3.js
@@ -9,90 +9,117 @@ ad3.factory('uniqueId', function () {
     return (id++);
   }
 });
-ad3.directive('d3Force', ['uniqueId', function (uniqueId) {
+ad3.factory('svgTag', ['uniqueId', '$q', function (uniqueId, $q) {
+  return function (elem, width, height) {
+    let svg = angular.element(elem).find("svg");
+    let randomID = $q.defer();
+    if (svg.length == 0) {
+      let id = "rand" + uniqueId();
+      elem.append("<svg id='" + id + "' width='" + width + "' height='" + height + "'></svg>");
+      randomID.resolve(id);
+    } else {
+      randomID.resolve(svg[0].id);
+    }
+    return randomID.promise;
+  };
+}]);
+ad3.directive('d3Force', ['svgTag', function (svgTag) {
+  let createDiagram = function ($scope, elem, attrs) {
+    let data = attrs.d3Force;
+    if (data === undefined)return;
+    data = JSON.parse(data);
+    if (data.nodes === undefined)
+      return;
+    const defaultW = 960;
+    const defaultH = 500;
+    const defaultCharge = 200;
+    const defaultLinkDist = 40;
+    let w = $scope.width = (attrs.width === undefined) ? defaultW : attrs.width;
+    let h = $scope.height = (attrs.height === undefined) ? defaultW : attrs.height;
+    let c = -defaultCharge;
+    if (attrs.charge !== undefined) {
+      let tmp = parseFloat(attrs.charge);
+      if (!isNaN(tmp)) {
+        c = -tmp;
+      }
+    }
+    let d = defaultLinkDist;
+    if (attrs.distance !== undefined) {
+      let tmp = parseFloat(attrs.distance);
+      if (!isNaN(tmp)) {
+        d = tmp;
+      }
+    }
+    let force = d3.layout.force().charge(c).linkDistance(d).size([w, h]);
+
+    let d3nodeMap = {};
+    $scope.links = data.links;
+    data.nodes.forEach(function (node) {
+      d3nodeMap[node.id] = node;
+    });
+    data.links.forEach(function (link) {
+      link.source = d3nodeMap[link.source];
+      link.target = d3nodeMap[link.target];
+    });
+    force.nodes(data.nodes).links(data.links).start();
+
+
+    svgTag(elem, w, h).then(function onComplete(uniqueId) {
+      let svg = d3.select("svg#" + uniqueId);
+      let link = svg.selectAll(".link")
+        .data(data.links)
+        .enter().append("line")
+        .attr("class", "link");
+
+      let node = svg.selectAll(".node")
+        .data(data.nodes)
+        .enter().append("circle")
+        .attr("class", "node")
+        .attr("r", 6)
+        .style("fill", function (d) {
+          return d.id;
+        })
+        .call(force.drag);
+      force.on("tick", function () {
+        link.attr("x1", function (d) {
+            return d.source.x;
+          })
+          .attr("y1", function (d) {
+            return d.source.y;
+          })
+          .attr("x2", function (d) {
+            return d.target.x;
+          })
+          .attr("y2", function (d) {
+            return d.target.y;
+          });
+
+        node.attr("cx", function (d) {
+            return d.x;
+          })
+          .attr("cy", function (d) {
+            return d.y;
+          });
+      });
+    });
+  };
   return {
     restrict: "A",
     link: function ($scope, elem, attrs) {
-      attrs.$observe('d3Force', function (data) {
-        data = JSON.parse(data);
-        if (data.nodes === undefined)
-          return;
-        const defaultW = 960;
-        const defaultH = 500;
-        const defaultCharge = 200;
-        const defaultLinkDist = 40;
-        let w = $scope.width = (attrs.width === undefined) ? defaultW : attrs.width;
-        let h = $scope.height = (attrs.height === undefined) ? defaultW : attrs.height;
-        let c = -defaultCharge;
-        if (attrs.charge !== undefined) {
-          let tmp = parseFloat(attrs.charge);
-          if (!isNaN(tmp)) {
-            c = -tmp;
-          }
-        }
-        let d = defaultLinkDist;
-        if (attrs.distance !== undefined) {
-          let tmp = parseFloat(attrs.distance);
-          if (!isNaN(tmp)) {
-            d = tmp;
-          }
-        }
-        let force = d3.layout.force().charge(c).linkDistance(d).size([w, h]);
-
-        let svg = angular.element(elem).find("svg");
-        let randomID = "rand" + uniqueId();
-        if (svg.length == 0) {
-          svg = elem.append("<svg id='" + randomID + "' width='" + w + "' height='" + h + "'></svg>");
-          svg = angular.element(elem).find("svg");
-        } else {
-          svg = svg[0];
-        }
-        let d3nodeMap = d3.map();
-        $scope.links = data.links;
-        data.nodes.forEach(function (node) {
-          d3nodeMap.set(node.id, node);
-        });
-        data.links.forEach(function (link) {
-          link.source = d3nodeMap.get(link.source);
-          link.target = d3nodeMap.get(link.target);
-        });
-        force.nodes(data.nodes).links(data.links).start();
-        svg = d3.select("svg#" + randomID);
-        let link = svg.selectAll(".link")
-          .data(data.links)
-          .enter().append("line")
-          .attr("class", "link");
-
-        let node = svg.selectAll(".node")
-          .data(data.nodes)
-          .enter().append("circle")
-          .attr("class", "node")
-          .attr("r", 6)
-          .style("fill", function (d) {
-            return d.id;
-          })
-          .call(force.drag);
-        force.on("tick", function () {
-          link.attr("x1", function (d) {
-              return d.source.x;
-            })
-            .attr("y1", function (d) {
-              return d.source.y;
-            })
-            .attr("x2", function (d) {
-              return d.target.x;
-            })
-            .attr("y2", function (d) {
-              return d.target.y;
-            });
-
-          node.attr("cx", function (d) {
-              return d.x;
-            })
-            .attr("cy", function (d) {
-              return d.y;
-            });
-        });
+      attrs.$observe('d3Force', function () {
+        createDiagram($scope, elem, attrs);
+      });
+      attrs.$observe('width', function () {
+        createDiagram($scope, elem, attrs);
+      });
+      attrs.$observe('height', function () {
+        createDiagram($scope, elem, attrs);
+      });
+      attrs.$observe('charge', function () {
+        createDiagram($scope, elem, attrs);
+      });
+      attrs.$observe('distance', function () {
+        createDiagram($scope, elem, attrs);
       });
     }
   }
