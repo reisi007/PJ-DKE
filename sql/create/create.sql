49d636c654bb54d7c99058fc17f6633ed9f6a9c1
Florian Reisinger <reisi007@gmail.com>
2016-03-18 09:48:23 +0100

    Added import for data

---
+++ sql/create/create.sql
@@ -0,0 +1,81 @@
+﻿CREATE TABLE pjdke.änderungshistorie
+(
+  aendernr  INT UNSIGNED PRIMARY KEY,
+  tabelle   TEXT,
+  feld      TEXT,
+  id        BIGINT UNSIGNED,
+  wertNeu   TEXT,
+  wertAlt   TEXT,
+  aenderts  TIMESTAMP,
+  aenderusr VARCHAR(16)
+);
+CREATE TABLE pjdke.bestellpos
+(
+  posnr         INT UNSIGNED,
+  bestellnr     BIGINT UNSIGNED,
+  materialnr    BIGINT UNSIGNED,
+  menge         INT UNSIGNED,
+  mengeneinheit VARCHAR(2),
+  preis         DOUBLE,
+  währung       VARCHAR(3),
+  stornokz      BOOLEAN,
+  erstelltTs    TIMESTAMP,
+  erstellusr    VARCHAR(16),
+  CONSTRAINT bestellpos_posnr_bestellnr_pk PRIMARY KEY (posnr, bestellnr)
+);
+CREATE TABLE pjdke.creditor
+(
+  kredNr    BIGINT UNSIGNED PRIMARY KEY,
+  vname     TEXT,
+  nname     TEXT,
+  firma     TEXT,
+  plz       INT UNSIGNED,
+  ort       TEXT,
+  land      VARCHAR(2),
+  SperrKZ   BOOLEAN,
+  erstellus VARCHAR(16),
+  erstellt  TIMESTAMP
+);
+CREATE TABLE pjdke.rechnung
+(
+  rechnungsnr    BIGINT UNSIGNED PRIMARY KEY,
+  positionsnr    INT UNSIGNED,
+  bestellnr      INT UNSIGNED,
+  eingangsdatum  TIMESTAMP,
+  rechnungsdatum DATE,
+  betrag         DOUBLE,
+  währung        VARCHAR(3),
+  kreditnr       BIGINT UNSIGNED
+);
+CREATE TABLE pjdke.bestellung
+(
+  bestellnr   BIGINT UNSIGNED PRIMARY KEY,
+  krednr      BIGINT UNSIGNED,
+  stornokz    BOOLEAN,
+  erstelltTs  TIMESTAMP,
+  erstellusr  VARCHAR(16),
+  freigabets  TIMESTAMP,
+  freigabeusr VARCHAR(16)
+);
+CREATE TABLE pjdke.wareneingang
+(
+  id            BIGINT UNSIGNED PRIMARY KEY,
+  posnr         INT UNSIGNED,
+  bestellnr     BIGINT UNSIGNED,
+  menge         INT UNSIGNED,
+  mengeneinheit VARCHAR(2),
+  eingangsts    TIMESTAMP,
+  eingangsurs   VARCHAR(16),
+  krednr        BIGINT UNSIGNED
+);
+CREATE TABLE pjdke.zahlung
+(
+  id          BIGINT UNSIGNED PRIMARY KEY,
+  rechnungsnr BIGINT UNSIGNED,
+  betrag      DOUBLE,
+  währung     VARCHAR(3),
+  zahlts      TIMESTAMP,
+  zahlusr     VARCHAR(16),
+  kreditornr  BIGINT UNSIGNED
+);
+COMMIT;
---
+++ sql/importer/src/at/reisisoft/jku/pjdke/sqlimporter/Main.java
@@ -0,0 +1,181 @@
+package at.reisisoft.jku.pjdke.sqlimporter;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    private static final String ÄNDERUNGSHISTORIE = "änderungshistorie";
+    private static final String BESTELLUNG = "bestellung";
+    private static final String BESTELLPOS = "bestellpos";
+    private static final String KREDITOR = "creditor";
+    private static final String RECHNUNG = "rechnung";
+    private static final String WARENEINGANG = "wareneingang";
+    private static final String ZAHLUNG = "zahlung";
+    private static final String JDBC_CONNECTION_STRING = "jdbc:mysql://localhost/pjdke?user=root&password=1234&useSSL=false";
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        log("Loading CSV");
+        Path dir = Paths.get("").toAbsolutePath();
+        final String[] filenames = {ÄNDERUNGSHISTORIE, BESTELLPOS, BESTELLUNG, KREDITOR, RECHNUNG, WARENEINGANG, ZAHLUNG};
+        final Path p = dir.getParent().resolve("csv");
+        Class.forName("com.mysql.jdbc.Driver");
+        Map<String, List<String[]>> fileContent = Arrays.stream(filenames).parallel().map(filename -> {
+            try (BufferedReader in = Files.newBufferedReader(p.resolve(filename + ".csv"))) {
+                List<String[]> values = new ArrayList<>();
+                String line;
+                while ((line = in.readLine()) != null) values.add(line.split(","));
+                return new AbstractMap.SimpleEntry<>(filename, values);
+            } catch (IOException e) {
+                return null;
+            }
+        }).filter(Objects::nonNull).collect(Collectors.toMap(AbstractMap.SimpleEntry::getKey, AbstractMap.SimpleEntry::getValue));
+
+        log("Get connection");
+        Connection con = DriverManager.getConnection(JDBC_CONNECTION_STRING);
+        con.setAutoCommit(false);
+        try (Statement s = con.createStatement()) {
+            for (String table : filenames) {
+                s.execute("truncate " + table);
+            }
+        }
+        List<String[]> rows = null;
+        log("Filling Änderungshistorie");
+        PreparedStatement preparedStatement = con.prepareStatement("INSERT INTO änderungshistorie VALUES (?,?,?,?,?,?,?,?)");
+        rows = fileContent.get(ÄNDERUNGSHISTORIE);
+        for (String[] line : rows) {
+            for (int i = 0; i < line.length; i++) {
+                preparedStatement.setString(i + 1, line[i]);
+            }
+            preparedStatement.addBatch();
+        }
+        preparedStatement.executeBatch();
+        preparedStatement.close();
+        log("Filling bestellpos");
+        preparedStatement = con.prepareStatement("INSERT INTO bestellpos VALUES (?,?,?,?,?,?,?,?,?,?)");
+        rows = fileContent.get(BESTELLPOS);
+        for (String[] line : rows) {
+            for (int i = 0; i < line.length; i++) {
+                switch (i) {
+                    case 7:
+                        preparedStatement.setBoolean(8, "x".equals(line[7]));
+                        break;
+                    default:
+                        preparedStatement.setString(i + 1, line[i]);
+                        break;
+                }
+            }
+            preparedStatement.addBatch();
+        }
+        preparedStatement.executeBatch();
+        preparedStatement.close();
+        log("Filling bestellung");
+        preparedStatement = con.prepareStatement("INSERT INTO bestellung VALUES (?,?,?,?,?,?,?)");
+        rows = fileContent.get(BESTELLUNG);
+        for (String[] line : rows) {
+            for (int i = 0; i < line.length; i++) {
+                switch (i) {
+                    case 2:
+                        preparedStatement.setBoolean(3, "x".equals(line[2]));
+                        break;
+                    default:
+                        preparedStatement.setString(i + 1, line[i]);
+                        break;
+                }
+            }
+            preparedStatement.addBatch();
+        }
+        preparedStatement.executeBatch();
+        preparedStatement.close();
+        log("Filling creditor");
+        preparedStatement = con.prepareStatement("INSERT INTO creditor VALUES (?,?,?,?,?,?,?,?,?,?)");
+        rows = fileContent.get(KREDITOR);
+        for (String[] line : rows) {
+            for (int i = 0; i < line.length; i++) {
+                switch (i) {
+                    case 0:
+                        preparedStatement.setLong(i + 1, Long.parseLong(line[i].substring(1)));
+                        break;
+                    case 7:
+                        preparedStatement.setBoolean(i + 1, line[7].length() == 1);
+                        break;
+                    default:
+                        preparedStatement.setString(i + 1, line[i]);
+                        break;
+                }
+            }
+            preparedStatement.addBatch();
+        }
+        preparedStatement.executeBatch();
+        preparedStatement.close();
+        log("Filling rechnung");
+        preparedStatement = con.prepareStatement("INSERT INTO rechnung VALUES (?,?,?,?,?,?,?,?)");
+        rows = fileContent.get(RECHNUNG);
+        for (String[] line : rows) {
+            for (int i = 0; i < line.length; i++) {
+                switch (i) {
+                    default:
+                        String tmp = line[i];
+                        if (tmp.isEmpty()) {
+                            tmp = null;
+                        }
+                        preparedStatement.setString(i + 1, tmp);
+                        break;
+                }
+            }
+            preparedStatement.addBatch();
+        }
+        preparedStatement.executeBatch();
+        preparedStatement.close();
+        log("Filling wareneingang");
+        preparedStatement = con.prepareStatement("INSERT INTO wareneingang VALUES (?,?,?,?,?,?,?,?)");
+        rows = fileContent.get(WARENEINGANG);
+        for (String[] line : rows) {
+            for (int i = 0; i < line.length; i++) {
+                switch (i) {
+                    default:
+                        String tmp = line[i];
+                        if (tmp.isEmpty()) {
+                            tmp = null;
+                        }
+                        preparedStatement.setString(i + 1, tmp);
+                        break;
+                }
+            }
+            preparedStatement.addBatch();
+        }
+        preparedStatement.executeBatch();
+        preparedStatement.close();
+        log("Filling zahlung");
+        preparedStatement = con.prepareStatement("INSERT INTO zahlung VALUES (?,?,?,?,?,?,?)");
+        rows = fileContent.get(ZAHLUNG);
+        for (String[] line : rows) {
+            for (int i = 0; i < line.length; i++) {
+                switch (i) {
+                    default:
+                        String tmp = line[i];
+                        if (tmp.isEmpty()) {
+                            tmp = null;
+                        }
+                        preparedStatement.setString(i + 1, tmp);
+                        break;
+                }
+            }
+            preparedStatement.addBatch();
+        }
+        preparedStatement.executeBatch();
+        preparedStatement.close();
+        con.commit();
+        log("Done");
+    }
+
+    private static void log(Object o) {
+        System.out.println(o);
+    }
+}
