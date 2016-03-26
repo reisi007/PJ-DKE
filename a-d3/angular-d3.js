00fcaec5356e3e421bd263318ae4e3ae2bd7ec38
Florian Reisinger <reisi007@gmail.com>
2016-03-26 08:04:40 +0100

    Added barchart

--- a-d3/angular-d3.js
+++ a-d3/angular-d3.js
@@ -4,22 +4,35 @@
 "use strict";
 let ad3 = angular.module('D3js', []);
 ad3.factory('uniqueId', function () {
-  let id = 0;
-  return function () {
-    return (id++);
-  }
+    let id = 0;
+    return function () {
+        return (id++);
+    }
 });
 ad3.factory('svgTag', ['uniqueId', '$q', function (uniqueId, $q) {
-  return function (elem, width, height) {
-    let svg = angular.element(elem).find("svg");
-    let randomID = $q.defer();
-    if (svg.length == 0) {
-      let id = "rand" + uniqueId();
-      elem.append("<svg id='" + id + "' width='" + width + "' height='" + height + "'></svg>");
-      randomID.resolve(id);
-    } else {
-      randomID.resolve(svg[0].id);
-    }
-    return randomID.promise;
-  };
+    return function (elem, width, height) {
+        let svg = angular.element(elem).find("svg");
+        let randomID = $q.defer();
+        if (svg.length == 0) {
+            let id = "rand" + uniqueId();
+            elem.append("<svg id='" + id + '\'' + (width !== undefined ? ( " width='" + width) : '') + (height !== undefined ? ("' height='" + height) : '') + "'></svg>");
+            randomID.resolve(id);
+        } else {
+            randomID.resolve(svg[0].id);
+        }
+        return randomID.promise;
+    };
+}]);
+ad3.factory('divTag', ['uniqueId', '$q', function (uniqueId, $q) {
+    return function (elem, onCreate, onUpdate) {
+        let div = angular.element(elem).find("div");
+        if (div.length == 0) {
+            let id = "rand" + uniqueId();
+            elem.append("<div id='" + id + "'></div>");
+            onCreate(id);
+        } else {
+            let id = (div[0].id);
+            onUpdate(id);
+        }
+    };
 }]);
--- a-d3/d3-bars.js
+++ a-d3/d3-bars.js
@@ -1,38 +1,13 @@
 /**
  * Created by Florian on 19.03.2016.
  */
-ad3.directive('d3Bar', ['svgTag', function (svgTag) {
-    let createDiagram = function ($scope, elem, attrs) {
-        const defaultW = 200;
-        const defaultH = 200;
-        let w = $scope.width = (attrs.width === undefined) ? defaultW : attrs.width;
-        let h = $scope.height = (attrs.height === undefined) ? defaultW : attrs.height;
-        let data = attrs.d3Data;
-        try {
-            data = JSON.parse(data);
-        } catch (e) {
-            console.log(e);
-            return;
-        }
-        /* console.log('Data-Bar');
-         console.log(data);*/
-        svgTag(elem, w, h).then(function (uniqueId) {
-            let svg = d3.select('#' + uniqueId);
-            //TODO d3 stuff
-        });
-    };
+
+ad3.directive('d3Bar', ['divTag', function (divTag) {
     return {
-        restrict: "E",
-        link: function ($scope, elem, attrs) {
-            attrs.$observe('d3Data', function () {
-                createDiagram($scope, elem, attrs);
-            });
-            attrs.$observe('width', function () {
-                createDiagram($scope, elem, attrs);
-            });
-            attrs.$observe('height', function () {
-                createDiagram($scope, elem, attrs);
-            });
+        restrict: 'E',
+        templateUrl: 'barchart.html',
+        scope: {
+            data: '=d3Data'
         }
     }
 }]);
