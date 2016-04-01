4573b6ea2e24f21c34b9211fb981d5f83f606f87
Florian Reisinger <reisi007@gmail.com>
2016-04-01 09:52:06 +0200

    Added onCreate / onUpdate to SVG

--- a-d3/angular-d3.js
+++ a-d3/angular-d3.js
@@ -9,30 +9,17 @@ ad3.factory('uniqueId', function () {
         return (id++);
     }
 });
-ad3.factory('svgTag', ['uniqueId', '$q', function (uniqueId, $q) {
-    return function (elem, width, height) {
+ad3.factory('svgTag', ['uniqueId', function (uniqueId) {
+    return function (elem, onCreate, onUpdate, width, height) {
         let svg = angular.element(elem).find("svg");
-        let randomID = $q.defer();
         if (svg.length == 0) {
             let id = "rand" + uniqueId();
             elem.append("<svg id='" + id + '\'' + (width !== undefined ? ( " width='" + width) : '') + (height !== undefined ? ("' height='" + height) : '') + "'></svg>");
-            randomID.resolve(id);
-        } else {
-            randomID.resolve(svg[0].id);
-        }
-        return randomID.promise;
-    };
-}]);
-ad3.factory('divTag', ['uniqueId', '$q', function (uniqueId, $q) {
-    return function (elem, onCreate, onUpdate) {
-        let div = angular.element(elem).find("div");
-        if (div.length == 0) {
-            let id = "rand" + uniqueId();
-            elem.append("<div id='" + id + "'></div>");
             onCreate(id);
         } else {
-            let id = (div[0].id);
+            let id = svg[0].id;
             onUpdate(id);
         }
     };
 }]);
+
