ce6bcc83496e0be48435b7936ebf553d2ae909aa
Florian Reisinger <reisi007@gmail.com>
2016-05-21 19:40:33 +0200

    merged Olivers D3 changes

--- a-d3/d3-graph.js
+++ a-d3/d3-graph.js
@@ -1,7 +1,7 @@
 /**
  * Created by Florian on 19.03.2016.
  */
-app.directive('d3Graph', function () {
+app.directive('d3Graph', ['filterFilter', function (filterFilter) {
     let createDiagram = function ($scope, elem, attrs) {
         let data = attrs.d3Data;
         let id = attrs.id;
@@ -11,15 +11,70 @@ app.directive('d3Graph', function () {
             console.log(e);
             return;
         }
-        if (data.length === 0) return;
+        if (data.length === 0 || data.nodes === undefined) return;
         let nodes = data.nodes, links = data.links, labelType = data.labelType;
         if (nodes.length === 0 || links.length === 0) {
             return;
         }
-        /*TODO d3
-         * nodes -> Array of nodes
-         * links -> Array of links
-         * */
+        let svg = d3.select('#' + id);
+
+        svg.selectAll("*").remove();
+
+        g = new dagreD3.graphlib.Graph()
+            .setGraph({})
+            .setDefaultEdgeLabel(function () {
+                return {};
+            });
+//TODO Oliver
+        nodes.forEach(function (d) {
+            let returnedData = filterFilter(nodes, function (element) {
+                console.log(element.node, d.node, element, d);
+                return element.node === d.node;
+            });
+            console.log("returnedData", returnedData, "nodes", nodes);
+            //Farben nach Knotentyp zuordnen
+            if (returnedData.length > 1) {
+                g.setNode(d.type, {label: d.type + " (" + d.cnt + ")", style: "fill: #FA9393"});
+            }//Wenn ein Typ Inner & End ist (evtl extra Farbe einführen: #EEC591)
+            else if (returnedData[0].nodetype == "start") {
+                g.setNode(d.type, {label: d.type + " (" + d.cnt + ")", style: "fill: #93FA97"});
+            }
+            else if (returnedData[0].nodetype == "end") {
+                g.setNode(d.type, {label: d.type + " (" + d.cnt + ")", style: "fill: #FA9393"});
+            }
+            else if (returnedData[0].nodetype == "inner") {
+                g.setNode(d.type, {label: d.type + " (" + d.cnt + ")"});
+            }
+        });
+
+        links.forEach(function (d) {
+            if (labelType == "cnt")
+                g.setEdge(d.from, d.to, {lineInterpolate: 'basis', label: d.cnt});
+            else if (labelType == "sec")
+                g.setEdge(d.from, d.to, {lineInterpolate: 'basis', label: d.deltaSec});
+        });
+
+        //Kanten der Nodes abrunden
+        g.nodes().forEach(function (v) {
+            var node = g.node(v);
+            node.rx = node.ry = 5;
+        });
+        let render = new dagreD3.render();
+        let svgGroup = svg.append("g");
+        render(d3.select("svg g"), g);
+
+
+        //Zentrieren
+        let offset = (svg.attr("width") - g.graph().width) / 2;
+        svgGroup.attr("transform", "translate(" + offset + ", 50)");
+
+
+        //Zoomen
+        d3.select("svg")
+            .call(d3.behavior.zoom().on("zoom", function () {
+                svg.select("g")
+                    .attr("transform", "translate(" + d3.event.translate + ") scale(" + d3.event.scale + ")");
+            }));
     };
     return {
         restrict: "A",
@@ -41,4 +96,4 @@ app.directive('d3Graph', function () {
             });
         }
     }
-});
+}]);
